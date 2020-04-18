f44b1ff8680ab5cc925ac8912096352ad86034d3
Samira <samirasaad@gmail.com>
2020-04-18 15:26:39 +0200

    rebuild search input component

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import InputField from './../Controls/TextField/TextField';
-
+import YoutubeSearchedForIcon from '@material-ui/icons/YoutubeSearchedFor';
+import './SearchInput.scss';
 const SearchInput = ({ handleChange,handleSubmit }) => {
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <InputField label='name'
-                    color='secondary'
-                    size='medium'
-                    handleChange={handleChange}
-                />
+            <form onSubmit={handleSubmit} className='form-wrapper  mt-2 text-center'>
+                <div className='position-relative'>
+                 <input className=' search-input'
+                  placeholder='Search for a photo'
+                  onChange={handleChange}
+                  />
+                 <span className='position-absolute search-icon'>
+                     <YoutubeSearchedForIcon color='secondary'/>
+                 </span>
+                </div>
             </form>
         </>
     )

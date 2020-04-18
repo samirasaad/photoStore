312f6eab43e35369ff9fc6a3882900d3bed294e6
Samira <samirasaad@gmail.com>
2020-04-18 21:30:40 +0200

    making home container , parent

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,16 +3,38 @@ import { connect } from 'react-redux';
 import SearchInput from '../../components/SearchInput/SearchInput';
 import { searchRequest } from './../../store/actions/search';
 import ImagesList from '../ImagesList/ImagesList';
+import Pagination from "react-js-pagination";
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      photosList: [],
+      activePage: 1,
+      photosPerPage: 20,
     }
   }
   componentDidMount = () => {
   }
+  componentDidUpdate = (prevProps) => {
+    const { results } = this.props;
+    if (prevProps.results !== this.props.results) {
+        this.setState({
+            photosList: results
+        })
+    }
+}
+
+
+handlePageChange = (pageNumber) => {
+  const { searchRequest } = this.props;
+  const { activePage, photosPerPage } = this.state;
+  this.setState({ activePage: pageNumber }, () => {
+      searchRequest({ query: this.state.searchTerm, page: activePage, per_page: photosPerPage })
+  })
+}
 
   handleChange = (e) => {
     this.setState({
@@ -31,11 +53,28 @@ class Home extends Component {
 
   render() {
     const { photosList, activePage, photosPerPage } = this.state;
+    const {total} = this.props;
     return (
       <>
         <SearchInput handleChange={this.handleChange}
           handleSubmit={this.handleSubmit} />
         <ImagesList photosList={photosList} />
+        {photosList.length > 0 &&
+                    <div className='my-4 w-100'>
+                        <Pagination
+                            className='justify-content-center'
+                            itemClass="page-item"
+                            linkClass="page-link"
+                            activePage={activePage}
+                            itemsCountPerPage={photosPerPage}
+                            totalItemsCount={total}
+                            pageRangeDisplayed={5}
+                            onChange={this.handlePageChange}
+                            prevPageText='Prev'
+                            nextPageText='Next'
+                        />
+                    </div>
+                }
       </>
     )
   }
@@ -43,9 +82,9 @@ class Home extends Component {
 
 const mapStateToProps = ({ locale: { lang }, search: { results, total, total_pages } }) => ({
   lang,
-  // results,
-  // total,
-  // total_pages,
+  results,
+  total,
+  total_pages,
 })
 
 export default connect(mapStateToProps, { searchRequest })(Home);
--- src/containers/ImagesList/ImagesList.js
+++ src/containers/ImagesList/ImagesList.js
@@ -3,21 +3,20 @@ import { connect } from 'react-redux';
 import ImageCard from '../../components/ImageCard/ImageCard';
 import './ImagesList.scss';
 import ImageModal from '../../components/ImageModal/ImageModal';
-import {downloadApPhotoRequest} from './../../store/actions/download';
-import { searchRequest } from './../../store/actions/search';
+import { downloadApPhotoRequest } from './../../store/actions/download';
+// import { searchRequest } from './../../store/actions/search';
 
-import Pagination from "react-js-pagination";
 
 class ImagesList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            photosList: [],
-            activePage: 1,
-            photosPerPage:20,
+            // photosList: [],
+            // activePage: 1,
+            // photosPerPage: 20,
             isOpen: false,
-            profile_image:null,
-            location:null,
+            profile_image: null,
+            location: null,
             imgObj: {
                 imgId: null,
                 img_description: '',
@@ -28,30 +27,15 @@ class ImagesList extends Component {
     componentDidMount = () => {
 
     }
-    componentDidUpdate = (prevProps) => {
-        const { results } = this.props;
-        if (prevProps.results !== this.props.results) {
-          this.setState({
-            photosList: results
-          })
-        }
-      }
+  
 
-      handlePageChange = (pageNumber) => {
-        const { searchRequest } = this.props;
-        const { activePage, photosPerPage } = this.state;
-        this.setState({ activePage: pageNumber }, () => {
-          searchRequest({ query: this.state.searchTerm, page: activePage, per_page: photosPerPage })
-        })
-      }
-
-    handleModalState = (imgId, img_description, imgUrl,profile_image, location) => {
+    handleModalState = (imgId, img_description, imgUrl, profile_image, location) => {
         const { isOpen } = this.state;
         !isOpen ?
             this.setState({
                 isOpen: true,
-                profile_image:profile_image,
-                location:location,
+                profile_image: profile_image,
+                location: location,
                 imgObj: {
                     ...this.state.imgObj,
                     imgId,
@@ -63,9 +47,9 @@ class ImagesList extends Component {
                 isOpen: false,
             })
     }
-   
-     forceDownload=(url)=>{
-         console.log(url);
+
+    forceDownload = (url) => {
+        console.log(url);
         //  debugger;
         // var url = link.getAttribute("data-href");
         // var fileName = link.getAttribute("download");
@@ -87,68 +71,48 @@ class ImagesList extends Component {
         // xhr.send();
     }
 
-    downloadImage= (id)=>{
-        const {downloadApPhotoRequest} =this.props;
+    downloadImage = (id) => {
+        const { downloadApPhotoRequest } = this.props;
         console.log(id);
         // downloadApPhotoRequest({id})
         // this.forceDownload()
     }
     render() {
-    const { total } = this.props;
-    // const { photosList } = this.props;
-        const { isOpen, imgObj,profile_image,location, photosList, activePage, photosPerPage } = this.state;
+        const { total } = this.props;
+        // const { photosList } = this.props;
+        const { isOpen, imgObj, profile_image, location, activePage, photosPerPage } = this.state;
+        const {photosList}=this.props;
         return (
             <section className='d-flex flex-row flex-wrap image-list-wrapper container-fluid my-4 '>
-                { photosList.length > 0 &&
-                    photosList.map(({ id, urls: { raw, full, regular, small, thumb },likes,
+                {
+                    photosList.map(({ id, urls: { raw, full, regular, small, thumb }, likes,
                         links: { self, html, download, download_location, liked_by_user },
                         description,
                         alt_description, user: { profile_image, name, location, instagram_username } }, index) => {
                         return (
                             <React.Fragment key={index}>
-                                {/* <div className='position-relative'>
-                                    <img className='m-2 img ' src={thumb} />
-                                </div>
-                                <div className='position-absolute'>test</div> */}
-                                {/* </div> */}
-                       <div className='card-wrapper d-flex justify-content-center'>
-                                <ImageCard
-                                    likes={likes}
-                                    regular={regular}
-                                    alt_description={alt_description}
-                                    liked_by_user={liked_by_user}
-                                    download={download}
-                                    download_location={download_location}
-                                    profile_image={profile_image.small}
-                                    name={name}
-                                    location={location}
-                                    instagram_username={instagram_username}
-                                    downloadImage={()=>this.downloadImage(id)}
-                                    forceDownload={()=>this.forceDownload(download)}
-                                    handleModalState={() => this.handleModalState(id, description, full,profile_image.small,location)}
-                                />
+                                <div className='card-wrapper d-flex justify-content-center'>
+                                    <ImageCard
+                                        likes={likes}
+                                        regular={regular}
+                                        alt_description={alt_description}
+                                        liked_by_user={liked_by_user}
+                                        download={download}
+                                        download_location={download_location}
+                                        profile_image={profile_image.small}
+                                        name={name}
+                                        location={location}
+                                        instagram_username={instagram_username}
+                                        downloadImage={() => this.downloadImage(id)}
+                                        forceDownload={() => this.forceDownload(download)}
+                                        handleModalState={() => this.handleModalState(id, description, full, profile_image.small, location)}
+                                    />
                                 </div>
                             </React.Fragment>
                         )
-                        
+
                     })
                 }
-                  {photosList.length > 0 &&
-                         <div className='my-4 w-100'>
-                         <Pagination
-                           className='justify-content-center'
-                           itemClass="page-item"
-                           linkClass="page-link"
-                           activePage={activePage}
-                           itemsCountPerPage={photosPerPage}
-                           totalItemsCount={total}
-                           pageRangeDisplayed={5}
-                           onChange={this.handlePageChange}
-                           prevPageText='Prev'
-                           nextPageText='Next'
-                         />
-                       </div>
-                  }
                 <ImageModal isOpen={isOpen}
                     // fullScreen={true}
                     handleModalState={this.handleModalState}
@@ -162,10 +126,10 @@ class ImagesList extends Component {
 }
 
 const mapStateToProps = ({ locale: { lang }, search: { results, total, total_pages } }) => ({
-        lang: lang,
-        results,
-        total,
-        total_pages,
+    lang: lang,
+    // results,
+    // total,
+    // total_pages,
 })
 
-export default connect(mapStateToProps, {downloadApPhotoRequest, searchRequest })(ImagesList);
+export default connect(mapStateToProps, { downloadApPhotoRequest })(ImagesList);

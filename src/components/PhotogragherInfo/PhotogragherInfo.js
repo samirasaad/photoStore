5d9b3ac864a8faf5d9dedb2254b6c9ca7ebcebf5
Samira <samirasaad@gmail.com>
2020-04-23 17:30:52 +0200

    adding profile data

--- src/components/PhotogragherInfo/PhotogragherInfo.js
+++ src/components/PhotogragherInfo/PhotogragherInfo.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import UserAvatar from '../UserAvatar/UserAvatar';
 import PinDropIcon from '@material-ui/icons/PinDrop';
+import './PhotogragherInfo.scss'
 
 const PhotogragherInfo = ({ userInfo }) => {
     const { first_name, last_name, location, bio } = userInfo
     console.log(userInfo)
     return (
         <section className='info-wrapper'>
-            <div className='d-flex justify-content-center'>
-                <div className='profile-img'>
+            <div className='row justify-content-center mx-0'>
+                <div className='col-md-2'>
                     <UserAvatar />
                 </div>
-                <div className='mx-2'>
+                <div className='col-md-5'>
                     <h3 className='font-weight-bold'>{first_name + '-' + last_name}</h3>
-
                     <span className='text-muted'>
                         <PinDropIcon color='secondary' />
                         {location}
                     </span>
-                    <p className='my-4'>{'-'+bio}</p>
-
+                   <p className='my-4'>{bio}</p>
                 </div>
             </div>
         </section>

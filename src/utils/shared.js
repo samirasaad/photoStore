a5c74f3f75d355896c21b792af63acfd1bf4414f
Samira <samirasaad@gmail.com>
2020-05-03 10:24:10 +0200

    removing shared file

--- src/utils/shared.js
+++
@@ -1,8 +0,0 @@
-import History from "./../routes/History";
-const getCollectionImages= (collection)=>{
-    History.push(`/imagesList/${collection}?page=1`)
-}
-
-export {
-    getCollectionImages
-}

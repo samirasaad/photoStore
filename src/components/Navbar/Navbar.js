025d60b9b3faabc3d87693942ce37f36f237ab91
Samira <samirasaad@gmail.com>
2020-05-03 21:29:52 +0200

    some ui enhancments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './Navbar.scss';
 import { Link } from 'react-router-dom';
+import logo from './../../assets/images/logo.jpg';
+import './Navbar.scss';
 const Navbar = () => {
   return (
-    <div className='nav-bar-wrapper mb-0'>
+    <nav className='nav-bar-wrapper mb-0'>
       <Link to='/'>
         <p className='p-3 font-weight-bold mb-0 pb-0'>
-          PhotoStore
+          PhotoStore <img src={logo}/>
         </p>
       </Link>
-    </div>
+    </nav>
   )
 }
 

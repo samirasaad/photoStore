f35582daea5f5307fc6c83bbfa3fee82ebb2c02a
Samira <samirasaad@gmail.com>
2020-05-12 15:39:18 +0200

    solving github pages issue

---
+++ build/precache-manifest.b8dafbf5d9150073f1892913d8476cd3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f002922240bf3dd60d68ddcb52315aa",
+    "url": "/photoStore/index.html"
+  },
+  {
+    "revision": "ca1d9a30ebeb52a2bbbe",
+    "url": "/photoStore/static/css/1.46c1e978.chunk.css"
+  },
+  {
+    "revision": "808b80141de464e4dd86",
+    "url": "/photoStore/static/css/5.2395384e.chunk.css"
+  },
+  {
+    "revision": "d5b2c8d5cf14e3c78605",
+    "url": "/photoStore/static/css/6.9f404b6f.chunk.css"
+  },
+  {
+    "revision": "f5342d9a647fd7bb874f",
+    "url": "/photoStore/static/css/7.7c9ba632.chunk.css"
+  },
+  {
+    "revision": "eaa1a689d77cab26acee",
+    "url": "/photoStore/static/css/main.d2cb6882.chunk.css"
+  },
+  {
+    "revision": "9ae5b1947a565da6d6c4",
+    "url": "/photoStore/static/js/0.7b5783ed.chunk.js"
+  },
+  {
+    "revision": "66bfcb5a1dba9b45cd3371b50d9d764d",
+    "url": "/photoStore/static/js/0.7b5783ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1d9a30ebeb52a2bbbe",
+    "url": "/photoStore/static/js/1.deae08b2.chunk.js"
+  },
+  {
+    "revision": "be094145ebf9d9ee971f",
+    "url": "/photoStore/static/js/4.3b6bb149.chunk.js"
+  },
+  {
+    "revision": "3c1eda373985a91243a1f35696505928",
+    "url": "/photoStore/static/js/4.3b6bb149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808b80141de464e4dd86",
+    "url": "/photoStore/static/js/5.dba91b81.chunk.js"
+  },
+  {
+    "revision": "d5b2c8d5cf14e3c78605",
+    "url": "/photoStore/static/js/6.47b7f367.chunk.js"
+  },
+  {
+    "revision": "f5342d9a647fd7bb874f",
+    "url": "/photoStore/static/js/7.3ce81704.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/photoStore/static/js/7.3ce81704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f77850529032ab92a8d",
+    "url": "/photoStore/static/js/8.541cb368.chunk.js"
+  },
+  {
+    "revision": "b799ad2cf1cecb89c02f",
+    "url": "/photoStore/static/js/9.67bd3157.chunk.js"
+  },
+  {
+    "revision": "eaa1a689d77cab26acee",
+    "url": "/photoStore/static/js/main.03c90b6f.chunk.js"
+  },
+  {
+    "revision": "b304ba3067f185474b4b",
+    "url": "/photoStore/static/js/runtime-main.f331cf5b.js"
+  },
+  {
+    "revision": "dc563b2b577a2836540b1ca94f867d5f",
+    "url": "/photoStore/static/media/404_page_not_found.dc563b2b.jpg"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/photoStore/static/media/PatrickHand-Regular.eeb4dad6.eot"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/photoStore/static/media/PatrickHand-Regular.eeb4dad6.svg"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/photoStore/static/media/PatrickHand-Regular.eeb4dad6.ttf"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/photoStore/static/media/PatrickHand-Regular.eeb4dad6.woff"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/photoStore/static/media/PatrickHand-Regular.eeb4dad6.woff2"
+  },
+  {
+    "revision": "931f944d0eba2043f417e19e7d0827a6",
+    "url": "/photoStore/static/media/homeHeader.931f944d.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/photoStore/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/photoStore/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/photoStore/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/photoStore/static/media/slick.f97e3bbf.svg"
+  }
+]);

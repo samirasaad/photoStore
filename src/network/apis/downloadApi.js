7bde4d4bfcd1d8875ee5fd529522b268da84279d
Samira <samirasaad@gmail.com>
2020-05-12 11:45:43 +0200

    clean up

--- src/network/apis/downloadApi.js
+++ src/network/apis/downloadApi.js
@@ -3,9 +3,6 @@ let handlerEnabled = true;
 const downloadAPhoto = async ({ id }) => {
     console.log(axiosInstance)
     return await axiosInstance.get(`https://api.unsplash.com/photos/${id}/download`, {
-        headers: {
-            Authorization: 'Client-ID PtJVadUoerKJguf5WxlQwRRevCUQPFuW-d5la9CKq_0'
-        },
         handlerEnabled
     }
   );
--- src/network/apis/searchApi.js
+++ src/network/apis/searchApi.js
@@ -4,9 +4,6 @@ const searchForPhotos = async params => {
     console.log(axiosInstance)
     return await axiosInstance.get(`search/photos`, {
         params,
-         headers: {
-            Authorization: 'Client-ID PtJVadUoerKJguf5WxlQwRRevCUQPFuW-d5la9CKq_0'
-        },
         handlerEnabled
     }
     );

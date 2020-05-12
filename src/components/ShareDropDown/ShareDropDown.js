89fd3849be9e747b39f02dc7be78e63507c93d72
Samira <samirasaad@gmail.com>
2020-05-12 21:57:23 +0200

    fixing sharing issue

--- src/components/ShareDropDown/ShareDropDown.js
+++ src/components/ShareDropDown/ShareDropDown.js
@@ -18,21 +18,6 @@ import { IconButton } from '@material-ui/core';
 import ShareIcon from '@material-ui/icons/Share';
 export default function ShareDropDown({ url }) {
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const [ShareButtons, setShareButtons] = React.useState(
-        [<FacebookShareButton 
-            children={<FacebookIcon size={32} round={`true`} />} url={url}
-         />,
-         <LinkedinShareButton 
-         children={<LinkedinIcon size={32} round={`true`} />} url={url} 
-         />,
-         <TwitterShareButton 
-         children={<TwitterIcon size={32} round={`true`} />} url={url} 
-         />,
-         <WhatsappShareButton 
-         children={<WhatsappIcon size={32} round={`true`} />} url={url} 
-         />
-        ]
-    )
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -52,14 +37,44 @@ export default function ShareDropDown({ url }) {
                 keepMounted
                 open={Boolean(anchorEl)}
                 onClose={handleClose}>
-                {ShareButtons.map((button, index) => {
-                    return (
-                        <MenuItem onClick={handleClose} key={index}>
-                            {button}
-                        </MenuItem>
-                    )
-                })
-                }
+                <MenuItem onClick={handleClose} >
+                    <FacebookShareButton
+                        children={<FacebookIcon 
+                        size={32}
+                        round={`true`}
+                        />}
+                        url={url}
+                    />
+                </MenuItem>
+                <MenuItem onClick={handleClose} >
+                    <LinkedinShareButton
+                        children={<LinkedinIcon
+                        size={32}
+                        round={`true`}
+                        />}
+                        url={url}
+                    />
+                </MenuItem>
+
+                <MenuItem onClick={handleClose} >
+                    <TwitterShareButton
+                        children={<TwitterIcon
+                        size={32}
+                        round={`true`}
+                        />}
+                        url={url}
+                    />
+                </MenuItem>
+
+                <MenuItem onClick={handleClose} >
+                    <WhatsappShareButton
+                        children={<WhatsappIcon
+                        size={32} 
+                        round={`true`}
+                        />}
+                        url={url}
+                    />
+                </MenuItem>
             </Menu>
         </div>
     );
